35ac96a7a7f58294b13f48b6e92e9892f9b2b240
ThalliaB <thalliabrum60@gmail.com>
2021-09-16 11:04:22 -0300

    Add oficial project of class

---
+++ ProjetoOficial/index.js
@@ -0,0 +1,10 @@
+var listaFilmes = [
+    "https://m.media-amazon.com/images/I/91FhSXlwgjL._AC_SL1500_.jpg",
+    "https://i.ytimg.com/vi/WwW1nqV3pI0/movieposter_en.jpg",
+    "https://capas-m2.wmixvideo.com.br/cr173495.jpg"
+  ];
+  
+  for (var i = 0; i < listaFilmes.length; i++) {
+    document.write("<img src=" + listaFilmes[i] + ">");
+  }
+  
